cd98ea83f61485a3ee5fb4fb502fcbed554bc605
infiniteltd <chrissamps24@gmail.com>
2024-01-04 14:33:02 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+
+
+  );
+}
+
---
+++ components/FeaturedRow.js
@@ -0,0 +1,40 @@
+import { View, Text, ScrollView } from 'react-native';
+import React from 'react';
+import { ArrowRightIcon } from 'react-native-heroicons/outline';
+import RestaurantCard from './RestaurantCard';
+
+const FeaturedRow = ({ id, title, description }) => {
+    return (
+        <View>
+            <View className="mt-4 flex-row items-center justify-between px-4">
+                <Text className="font-bold text-lg">{title}</Text>
+                <ArrowRightIcon color="#00CCBB" />
+            </View>
+            <Text className="text-xs text-gray-500 px-4">{description}</Text>
+            <ScrollView
+                horizontal
+                contentContainerStyle={{
+                    paddingHorizontal: 15,
+                }}
+                showsHorizontalScrollIndicator={true}
+                className="pt-4"
+            >
+                {/* RestaurantCards */}
+                <RestaurantCard
+                    id={1}
+                    imgUrl="https://links.papareact.com/gn7"
+                    title="The Sushi"
+                    rating={4.8}
+                    genre="French"
+                    address="124 Main St. ave "
+                    short_description="This is a description"
+                    dishes={[]}
+                    long={25}
+                    lat={5}
+                />
+            </ScrollView>
+        </View>
+    );
+};
+
+export default FeaturedRow;
